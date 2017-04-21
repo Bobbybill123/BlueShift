07384dce2ecfb719713b0c4e524cc2eeb1629e29
sophi <sophie.cotterill@gmail.com>
2017-04-21 22:05:52 +1200

    Player and Block collisions finished. Feel free to improve

--- src/main/java/BlueShift/entity/Block.java
+++ src/main/java/BlueShift/entity/Block.java
@@ -56,12 +56,21 @@ public class Block extends Entity {
 	@Override
 	public void checkCollision(Entity other) {
         //LeannanSC apologises in advance for the terrible code :P
-        //Block hitbox dimensions (top, left, width, height)
+        //Block hitbox dimensions (position.x, position.y, width, height)
+        //Player hitbox dimensions (main.player.getPosition.x/y , main.player.getWidth/getHeight)
         //Player can move up through the hitbox but not down
+        //the player must be moving down to 'walk' on the block (??vertical velocity positive??)
 
         //Identifies when player is on the block
+        boolean playerBlockCollision = false;
         main.player.getBounds();
-        //if ();
+        PVector playerPos = main.player.getPosition();
+        if ((playerPos.x + main.player.getWidth()) < position.x && (playerPos.x > (position.x + width)) && ((playerPos.y + main.player.getHeight()) == position.y)) {
+            if (main.player.getVelocity().y >= 0)
+                playerBlockCollision = true;
+        }
+        if (playerBlockCollision)
+            System.out.println("I DID IT!!!");
 	}
 
 
