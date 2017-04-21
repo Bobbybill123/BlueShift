ff4558a736202c7f84452fd9661c078fc526ffb9
dknz2008 <dylan.kumar@icloud.com>
2017-04-21 18:48:31 +1200

    Created an Entity class which deals with collisions.

---
+++ src/main/java/entity/Entity.java
@@ -0,0 +1,28 @@
+package entity;
+
+import processing.core.PVector;
+
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Created by TML_TEST on 21/04/2017.
+ */
+public abstract class Entity {
+
+    abstract float getWidth();
+    abstract float getHeight();
+    abstract PVector getPosition();
+
+    Rectangle2D.Float getBounds() {
+        return new Rectangle2D.Float(getPosition().x, getPosition().y, getWidth(), getHeight());
+    }
+
+    boolean RectangleIntersection(Entity other) {
+        return (getBounds().intersects(other.getBounds()));
+    }
+
+    boolean pointIntersection(PVector point){
+        return(getBounds().contains(point.x, point.y));
+    }
+
+}
