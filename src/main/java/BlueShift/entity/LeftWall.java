00486060d857b0abfe3f1dfe8b8c4d1fb5cf0f63
Bobbybill123 <jacob.cohn.gell@gmail.com>
2017-04-23 13:49:25 +1200

    Got left wall animations working, added audio to resources

--- src/main/java/BlueShift/entity/LeftWall.java
+++ src/main/java/BlueShift/entity/LeftWall.java
@@ -3,14 +3,15 @@ package BlueShift.entity;
 import BlueShift.Main;
 import processing.core.PVector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by TML_TEST on 21/04/2017.
  */
 public class LeftWall extends Entity {
-
 	public static Animation sprite;
+	private List<Animation> animations = new ArrayList<>();
 	private Main main;
 	private PVector position;
 
@@ -36,10 +37,10 @@ public class LeftWall extends Entity {
 
 	@Override
 	public void draw() {
-		for (int i = 0; i < main.height; i+=(main.height/(getHeight()/sprite.getHeight()))) {
-			if(main.frameCount % 10 == 0)
-				sprite.animate(position.x, position.y + i);
-			else sprite.displayCurr(position.x, position.y + i);
+		if(main.frameCount % (10 - ((int) (main.gameSpeed / 5))) == 0) {
+			animations.forEach(animation -> animation.animate(0, animations.indexOf(animation) * animation.getHeight()));
+		} else {
+			animations.forEach(animation -> animation.displayCurr(0, animations.indexOf(animation) * animation.getHeight()));
 		}
 	}
 
@@ -55,6 +56,10 @@ public class LeftWall extends Entity {
 
 	public void setupSprite() {
 		sprite.resize(100, 0);
-
+		for (int i = 0; i < main.height; i+=(main.height/(getHeight()/sprite.getHeight()))) {
+			Animation animation = sprite.copy();
+			animations.add(animation);
+			animation.setFrame((int) main.random(animation.getImageCount()));
+		}
 	}
 }
